eb8de0bf1d94f855b4893a368a87d6364d77ac06
eshat002 <eshatjubayer22@outlook.com>
2023-11-29 18:47:39 +0600

    added requirements.txt

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-47x^*igr7%o+!ji#6_ea$@@ggx=o_t7p-yeq34%f%rz2iczxhj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
