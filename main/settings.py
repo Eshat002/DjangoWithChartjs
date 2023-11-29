69c7effb3035eaa5b67e0e6018e1ed3bc830dd4c
Shahriar Jubayer Eshat <123627627+Eshat002@users.noreply.github.com>
2023-11-29 19:00:52 +0600

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-47x^*igr7%o+!ji#6_ea$@@ggx=o_t7p-yeq34%f%rz2iczxhj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
